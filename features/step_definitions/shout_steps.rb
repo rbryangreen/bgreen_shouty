758c3ad9008fba034a4c6b18b455bcde95ff0bfb
Bryan Green <bryangreen82@gmail.com>
2018-03-21 10:35:09 -0400

    refactor

--- features/step_definitions/shout_steps.rb
+++ features/step_definitions/shout_steps.rb
@@ -11,12 +11,12 @@ Given('{word} is at {int}, {int}') do |name, x, y|
   @shouty.set_location(name, Coordinate.new(x, y))
 end
 
-When("Oscar shouts") do
-  @shouty.shout('Oscar', ARBITRARY_MESSAGE)
+When("{word} shouts") do |name|
+  @shouty.shout(name, ARBITRARY_MESSAGE)
 end
 
-When('Sean shouts') do
-  @shouty.shout("Sean", ARBITRARY_MESSAGE)
+When('{word} shouts') do |name|
+  @shouty.shout(name, ARBITRARY_MESSAGE)
 end
 
 Then('{word} should hear {word}') do |name1, name2|
@@ -27,6 +27,6 @@ Then('{word} should not hear {word}') do |name1, name2|
   expect(@shouty.shouts_heard_by(name1).include?(name2)).to be false
 end
 
-Then('Lucy should hear nothing') do
-  expect(@shouty.shouts_heard_by("Lucy").size).to eq(0)
+Then('{word} should hear nothing') do |name|
+  expect(@shouty.shouts_heard_by(name).size).to eq(0)
 end
